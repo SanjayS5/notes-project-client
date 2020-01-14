1b65d1c4180b5c0b4fbba427b539df39c99e7f06
Sanjay Sudhakaran <34014570+SanjayS5@users.noreply.github.com>
2020-01-14 16:26:18 +1300

    Setting up the React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "ap-southeast-2",
+      BUCKET: "notes-portfolio-project"
+    },
+    apiGateway: {
+      REGION: "ap-southeast-2",
+      URL: "https://yb60odad1f.execute-api.ap-southeast-2.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "ap-southeast-2",
+      USER_POOL_ID: "ap-southeast-2_0xV5YnjTU",
+      APP_CLIENT_ID: "143846mshpfn9n48f3o320p65q",
+      IDENTITY_POOL_ID: "ap-southeast-2:9913ffda-5ca6-4f98-9932-ab0ddd0b4797"
+    }
+  };
